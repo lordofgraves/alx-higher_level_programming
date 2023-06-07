b21ae8edb85e531c941fe94fba523dbb727922da
lordofgraves <achrafboutzate@gmail.com>
2023-06-07 14:46:38 +0000

    mm

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for fizzbuzz in range(0, 101, 1):
-        if fizzbuzz / 3 == 0:
+        if fizzbuzz % 3 == 0:
             print("Fizz ", end = '')
-        elif fizzbuzz / 5 == 0:
+        elif fizzbuzz % 5 == 0:
             print("buzz ", end = '')
         else:
             print("{:d} ".format(fizzbuzz), end = '')
