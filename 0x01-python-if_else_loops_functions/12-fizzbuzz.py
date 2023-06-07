67508181235e1bbb74cf6e62d0cdcecd34245941
lordofgraves <achrafboutzate@gmail.com>
2023-06-07 15:06:01 +0000

    mm

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fizzbuzz in range(1, 101, 1):
-        if fizzbuzz % 3 == 0:
-            print("Fizz ", end = '')
-        elif fizzbuzz % 5 == 0:
-            print("buzz ", end = '')
-        else:
-            print("{:d} ".format(fizzbuzz), end = '')
-    return (fizzbuzz)
+    for fizzbuzz in range(0, 101, 1):
+    if fizzbuzz % 3 == 0:
+        print("Fizz ", end = '')
+    elif fizzbuzz % 5 == 0:
+        print("Buzz ", end = '')
+    elif fizzbuzz % 5 == 0 and fizzbuzz % 3 == 0:
+        print("FizzBuzz")
+    else:
+        print("{:d} ".format(fizzbuzz), end = '')
