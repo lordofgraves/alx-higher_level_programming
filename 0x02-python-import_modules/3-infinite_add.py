1ca2a967240df7df2f4dded977b4f4ff40486c94
lordofgraves <achrafboutzate@gmail.com>
2023-06-08 17:10:42 -0700

    mm

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys
-
-    total = 0
-    for a in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    from sys import argv
+    arg_len = len(argv)
+    if arg_len == 1:
+        print(0)
+    elif arg_len > 1:
+        res = 0
+        for x in range(1, arg_len):
+            res = res + int(argv[x])
+        print("{:d}".format(res))
