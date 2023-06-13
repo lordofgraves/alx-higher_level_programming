b9d03548a0fffec9162640bd2728b30a6b7258ba
lordofgraves <achrafboutzate@gmail.com>
2023-06-13 02:47:30 +0000

    mm

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    cr = "cC"
     new_string = ""
     for char in my_string:
-        if char != cr:
-            new_string += char
+        if char not in "cC":
+            new_string = new_string + char
     return new_string
