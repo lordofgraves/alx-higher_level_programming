8f4ac5bc99dbbef446b3194304ae8f326615da85
lordofgraves <achrafboutzate@gmail.com>
2023-06-26 16:35:41 +0000

    mm

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     c = 0
     for i in range(x):
         try:
-                print("{}".format(my_list[i]), end="")
-                c += 1
+            print("{}".format(my_list[i]), end="")
+            c += 1
         except IndexError:
             continue
     print("")
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in range(x):
         try:
-                print("{:d}".format(my_list[i]), end="")
-                c += 1
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
         except (TypeError, ValueError):
             continue
     print()
