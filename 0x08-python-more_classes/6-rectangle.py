4f035bf817da20025ac36259cf7f478db16a24cd
lordofgraves <achrafboutzate@gmail.com>
2023-11-29 05:26:52 -0800

    cc

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,9 @@ class Rectangle:
         width (int)
         height (int)
     """
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """creates new instances of Rectangle.
 
